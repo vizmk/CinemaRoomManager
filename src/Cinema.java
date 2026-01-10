a7ffd7d17f5e6e574b6c5979fda562c4201058d5
vincenzocavallaro <vincenzo.cavallaro98@gmail.com>
2026-01-10 16:13:18 +0100

    Fase 4 CinemaRoom

--- src/Cinema.java
+++ src/Cinema.java
@@ -1,36 +1,54 @@
 import java.util.Scanner;
 
+public class Cinema {
 
-    public class Cinema {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Logic logic = new Logic();
 
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            Logic logic = new Logic();
+        System.out.println("Enter the number of rows:");
+        int rows = scanner.nextInt();
 
-            System.out.println("Enter the number of rows:");
-            int rows = scanner.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        int seats = scanner.nextInt();
 
-            System.out.println("Enter the number of seats in each row:");
-            int seats = scanner.nextInt();
+        logic.init(rows, seats);
 
+        boolean exit = false;
+
+        while (!exit) {
             System.out.println();
-            logic.init(rows, seats);
-            logic.print();
+            System.out.println("1. Show the seats");
+            System.out.println("2. Buy a ticket");
+            System.out.println("0. Exit");
 
-            System.out.println();
-            System.out.println("Enter a row number:");
-            int rowNumber = scanner.nextInt();
+            int choice = scanner.nextInt();
 
-            System.out.println("Enter a seat number in that row:");
-            int seatNumber = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println();
+                    logic.print();
+                    break;
 
-            System.out.println();
-            int price = logic.ticketPrice(rowNumber);
-            System.out.println("Ticket price: $" + price);
+                case 2:
+                    System.out.println();
+                    System.out.println("Enter a row number:");
+                    int rowNumber = scanner.nextInt();
 
-            System.out.println();
-            logic.book(rowNumber, seatNumber);
-            logic.print();
+                    System.out.println("Enter a seat number in that row:");
+                    int seatNumber = scanner.nextInt();
+
+                    int price = logic.ticketPrice(rowNumber);
+                    System.out.println();
+                    System.out.println("Ticket price: $" + price);
+
+                    logic.book(rowNumber, seatNumber);
+                    break;
+
+                case 0:
+                    exit = true;
+                    break;
+            }
         }
     }
-
+}
